503eeda644087704aa54d478d15f7e13e1af4abc
jeremy <jeremyaleman468@gmail.com>
2023-09-01 17:56:04 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+    return a + b;
+    // we'll fill this in shortly
+  }
+
+  function subtract(a, b) {
+    return a - b;
+    // we'll fill this in shortly
+  }
+
+
+  function multiply(a, b) {
+    return  a * b;
+    // we'll fill this in shortly
+  }
+
+
+  function divide(a, b) {
+    return a / b;
+    // we'll fill this in shortly
+  }
+
+  function increment(n) {
+    return++n; 
+  }
+
+
+  function decrement(n) {
+    return--n; 
+  }
+  
+
+  function makeInt(n) {
+     return  parseInt(n , 10); 
+  }
+
+  function preserveDecimal(n) {
+    return parseFloat(n)
+  }
